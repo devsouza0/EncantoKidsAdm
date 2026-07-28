e97943cc9f3eb94f679c24f34a4a3d86503b7a2c
devsouza0 <viniciusgsouza200600@gmail.com>
2026-07-28 17:14:07 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,8 @@
+const supabaseUrl = "https://stnjpquafragqjwxyjza.supabase.co";
 
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0bmpwcXVhZnJhZ3Fqd3h5anphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5MzI2MzAsImV4cCI6MjEwMDUwODYzMH0.r3YlozqWb2h96TLomC-VOfeQNLknio8x5xaiNVlQOkE";
+
+const supabaseClient = window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
